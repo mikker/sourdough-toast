8928024acc4be976f291213947c28a5d00508bff
Mikkel Malmberg <mikkel@brnbw.com>
2024-03-16 16:16:26 +0100

    Update styles a bit, update example, add Readme

--- src/sourdough-toast.js
+++ src/sourdough-toast.js
@@ -17,7 +17,7 @@ let toastsCounter = 0;
 // {{{ Helpers
 const SVG_NS = "http://www.w3.org/2000/svg";
 
-const svgTags = ["svg", "path", "line"];
+const svgTags = ["svg", "path", "line", "circle"];
 
 const h = (tag, props = {}, children = [], isSvg = false) => {
   const element =
@@ -65,38 +65,31 @@ const svgAttrs = {
   viewBox: "0 0 24 24",
   height: "20",
   width: "20",
-  fill: "currentColor",
+  fill: "none",
+  stroke: "currentColor",
+  "stroke-width": "1.5",
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round",
   dataset: { slot: "icon" },
-};
-const pathRules = {
-  "fill-rule": "evenodd",
-  "clip-rule": "evenodd",
 };
 
 const icons = {
-  success: h("svg", svgAttrs, [
-    h("path", {
-      d: "M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z",
-      ...pathRules,
-    }),
-  ]),
+  success: h("svg", svgAttrs, [h("path", { d: "M20 6 9 17l-5-5" })]),
   info: h("svg", svgAttrs, [
-    h("path", {
-      d: "M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z",
-      ...pathRules,
-    }),
+    h("circle", { cx: "12", cy: "12", r: "10" }),
+    h("path", { d: "M12 16v-4" }),
+    h("path", { d: "M12 8h.01" }),
   ]),
   warning: h("svg", svgAttrs, [
     h("path", {
-      d: "M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z",
-      ...pathRules,
+      d: "m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3",
     }),
+    h("path", { d: "M12 9v4" }),
+    h("path", { d: "M12 17h.01" }),
   ]),
   error: h("svg", svgAttrs, [
-    h("path", {
-      d: "M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z",
-      ...pathRules,
-    }),
+    h("circle", { cx: "12", cy: "12", r: "10" }),
+    h("path", { d: "m4.9 4.9 14.2 14.2" }),
   ]),
   spinner: h(
     "svg",
